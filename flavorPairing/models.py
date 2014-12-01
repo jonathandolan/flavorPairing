ffb6b2fb335d30d3fc36f89d5c121fb7699aaaa6
jonathandolan <jonathanddolan@gmail.com>
2014-12-01 16:18:35 -0700

    Added preliminary models based on EER. Changed db settings to connect to MySql database on localhost:3306.

--- flavorPairing/models.py
+++ flavorPairing/models.py
@@ -1,3 +1,95 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    recipeID = models.IntegerField(primary_key=True)
+    recipeName = models.CharField(max_length=45)
+    recipeDesc = models.CharField(max_length=45)
+    recipeInstr = models.CharField(max_length=45)
+
+
+class Ingredient(models.Model):
+    ingredientID = models.IntegerField(primary_key=true)
+    ingredientName = models.CharField(max_length=45)
+    ingredientDesc = models.CharField(max_length=45)
+    ingredientCategory = models.CharField(max_length=45)
+    ingredientAroma = models.CharField(max_length=45)
+    pairingID = models.ForeignKey(Pairing)
+    cuisineID = models.ForeignKey(Cuisine)
+
+
+class Bakery(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    bakeryType = models.CharField(max_length=45)
+    bakeryGlutenFree = models.IntegerField()
+
+
+class Dairy(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    dairyType = models.CharField(max_length=45)
+
+
+class Fruit(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    fruitClass = models.CharField(max_length=45)
+    fruitFamily = models.CharField(max_length=45)
+
+
+class Vegatables(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    vegFamily = models.CharField(max_length=45)
+    vegCategory = models.CharField(max_length=45)
+
+class Oil(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    oilSolid = models.IntegerField()
+    oiFatType = models.CharField(max_length=45)
+    oilSmokePoint = models.IntegerField()
+
+
+class Meat(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    meatCut = models.CharField(max_length=45)
+    meatCookStyle = models.CharField(max_length=45)
+
+
+class Beverage(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    alcoholic = models.IntegerField()
+    beverageAbv = models.CharField(max_length=45)
+    percentage = models.IntegerField()
+    rating = models.IntegerField()
+
+
+class Spice(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    spiceCuisine = models.CharField(max_length=45)
+    spiceGroup = models.CharField(max_length=45)
+
+
+class NutsAndSeeds(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    classification = models.CharField(max_length=45)
+    shell = models.IntegerField()
+
+
+class Cuisine(models.Model):
+    cuisineID = models.IntegerField(primary_key=True)
+    cuisineName = models.CharField(max_length=45)
+    cuisineDesc = models.CharField(max_length=45)
+    cuisineRegion = models.CharField(max_length=45)
+
+class HealthRating(models.Model):
+    ingredientID = models.ForeignKey(Ingredient)
+    diet = models.CharField(max_length=45)
+    score = models.IntegerField()
+    reason = models.CharField(max_length=45)
+
+
+class Pairing(models.Model):
+    pairingID = models.IntegerField(primary_key=True)
+    ingredientID = models.ForeignKey(Ingredient)
+    ingredientID2 = models.ForeignKey(Ingredient)
+    strength = models.IntegerField()
+    rating = models.IntegerField()
