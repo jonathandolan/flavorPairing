2654e9dcb339130b9a4d068ca772de69d76dd825
cfowles <chris.fowles@gmail.com>
2014-12-03 19:11:37 -0700

    Bootstrap 3 for admin pages.

--- flavorPairingProject/settings.py
+++ flavorPairingProject/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped.bootstrap3',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'flavorPairing',
     'bootstrap3',
+
 )
 
 MIDDLEWARE_CLASSES = (
